3dda1036d9235c7234edf5647b778c063c83ba14
Anon_01 <57970996+ElCheGuapo@users.noreply.github.com>
2021-11-22 23:46:07 -0800

    _wip

---
+++ sketch.js
@@ -0,0 +1,108 @@
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNNNNNNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0OOOkkOkkdddooodxOKNWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXOkkkkkkxollllllloolclllllllldxxkkO0KXNNNXXXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKxccclcccllllc::coooolllcllc::cloooooloodddddodxOKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXKOoclll:;;:cccccclloddoooooolc:::cllooolcloooooooooodxO000000KXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0kdolc:lol:;;:cccccclloooooolc:;:clllooolooocccllollllllolooollcloddxOKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNkolc:cc::coollccc::;;;;;;;;;;;,'..',clldxoclol;;;;;:::clcccccclc:;;;cc:;:ldx0NMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXxllc;,''..',;;:cccc:;;;;,,,,,,;;,,,,,:odddolc::cl:;;;;::coolclooooolcccc;,',cOWMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXxl:;::;,,,;;;,',;:clllclloooc;;;,,;cc;;:::,,,,;codkkxl;'',:coo::clooooddoooollOWMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKocc:;,,:c:::ccclllllllccooddoc,,:,..,c,......',coxxxxdl;..'::ldc,,,;:cclooodxxx0WMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKo:clcc;;:cccc::::cc::cloddool:'..::,;::,,,',:,,:ldxdolll:'';clodl,,:;,,;::ccc::lOWMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0l;coolc;:lol;,;;;,;::cllllc;'.',,:llooooolc:;,'';clol::ldoc;:cloo;.';:;,,,,;:,':kNMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKo::ccc:;;clc,';:,,,,,''.',:c;..;;:oddxddoc::::'',';cc;;:ldoloc:cc;''',,,;;',:oxOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0dol:,,;;;;;:,';:,;::;;,,';:cdd:'.;loddddl:,,,,'''..';,.':cllloolcc,,;;;,,,;:dXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0xllc;''',;;::;,;:::ccc:;,,;cloddl'.;loooolcccll:,''','''..,;;:::::llccllllc;;xNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXdcc:'';::;,',:cllc;;cclllcccllooooc;;:cclllooddoc:;,''';;;,''',,;:cccloooodddlxXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKo;'.':cc:;,....;clc,';cclloollccll;;:::cllloddlclodoc,..,::::;;;cloooddoooodxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNkc:::::::;;,'..':c;..';:cllcccc:;,;::cccllll:;;;;;::;,,;;;;:ccloooooddoooddx0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNkolcccclllllc:;,;:;. ..',,;;;;;,;:,,;:::::;'',....,::clollllllooooddddddddddxKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNkllc:clooddddolc:;'..','''..''',;;,''''',;;,';c;;::cclloooooooooodoool:;;cooodKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXdll:;;::ccccccllc;,;,......','...,;;::ccccc:;lollooooollllllllcllllc;,'..,cdOKNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWkcc:;;;;;::c::;::,;c,';;,',,,;''',;::cc::ccccloooddddddodolc::::cll:,,,;:coKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNx:;;;;,''',:clc,.;c,':c:'..',,;;::::cc::oodoodddxxxxdddxxdolc::ccc:;:clood0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNkc,,''....,;:c'.,c;,c;.....;cc::cc:llllooooodddxxxxxddxxxxdolccc:;:llloodxXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKo:;;;;,;:::;'',lc;c:..',,:lc::cccclllcllooooddddddddxxxxxxdolllcloooooodONMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0l,'.',,;:::;,;cccllccc::cc:cccllllccllloooooooodddddxxkkxddoodddoooooox0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWk:;..''',;;;,..':clodddooc:::;:lcccllllllooooooooddddxxkxxxdddoccllooodkXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWkc;,;:c:;;,,.....';:clllc;;:clllllooooolooooooooooodddxxxxdddc;:odddoddk0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXo''clc:,'''''..';;;;,,,,;:cloooooooooooollloooooooodddxxdddc,,:lcccclodkNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWO:,;cc:,...,,','';:cc:;;;:cllloooooooooollooooooooooddddddddo:::;;ldodxkKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0c,,,;;,,'.''',;:cllll:;::cloooooooooooooooooooooooooddddxxdddoollOWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWO:,,:cc;,..',,;clloolc:::clooooooooooollooooooooooooodddxocclllllkNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0l,,;,'......,cllooolc::cclllllllllllllloooooooooooooddoc;;:cccdKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXd;,,,,,,'.';clooooolc::::cccclllllllllllloooooooooooddoooolloONMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWOc,,,'''',;clodddollc:::::cccccclllclllllllooooooooddddollo0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKl,,'..,,;clodxdoolcccc::::::ccccccccclllllllooooodddxxxxxKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKo;'.',,;clodxxdollllllccc:::::::::::cccccclllooooddddxxxKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNOl,',;;cloddddooloooolllccccc:::;;;;;;;;::ccccllloodxOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNx:;;;:loodddoooooooooollllllcc:;;;;;:::d0KKKKKKKXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc;;;:cloodddooooooooooooooolllcccllooldXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0c;:;:cloodddddoooooooooooooooooooddddooOWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd:::;:clloodddddooooooooddddddddddddddodx0NNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWN0o::c::;:clooodddddooooooooooddddddooooodxxdolldkO0KNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWN0xl:;;::c:::cllooodddddooooooooooddddddoddddxxo:;,;looodxkO0KXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNKOdl:;;:;;;:::::ccllooodddoooooodddddddddddoodxkxxo:,,;cooooooooodxk0NWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0xoc:::::::;;;:ccccccclloooooooooooooooddooooooddxkxdl;,,,;looooooooooodxkO0KXNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0kdc:::cccccc::;;;:cccclcccloooodolllllllllooollooodxxxdol;,,,,cooooooooooodddlloodxkO0KXNWWMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKkdlc:::ccccccccc::;;:ccccllc::cloddddocccccccllllloodddxxxdoc,,;,';loooooooooodddolcclllllodxkkOKXNWMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMWX0kdlc:::cccccccccccc:::::cccccll::::clodddoc::::cccccllodddddddooc,,,,,,:odooooooooooodolclloooddddddxxkOKXWMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMWN0kdl:::::::cccccccccccc::::cccccccll:;:::coddxo:;;:::cccllodddddoooooc;,,,,';lddoooooooodddddolooodddddddxxxxxxO0NWMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMWXOxoc:ccc::::::::::::::::::::ccccccccllc:;:::cldxxo:;:cclllccldxxdolllool:;,;;,,,:oddooooooooooddolloooooddddddddxxdxxOXWMMMMMMMMMMM
+// MMMMMMMMMMMMMMWKkoccccccccccccc::::::::::::::cccc:cccclll:;:::::cdxxd:;clllllclodxdlcccloolc::::;;,;codoooooooooooddlcloooodddddddddddxdxxOXWMMMMMMMMM
+// MMMMMMMMMMMMN0dccccclllllllcccclcccccccccccccc::::ccccll:;;:::::codxdl;;:cccclodxdoc::clooolllllcc:;:ldddoooooooooodoccllooddddddddddddxxxdkKWMMMMMMMM
+// MMMMMMMMMMW0dc:ccclllllllllccccllllooooooooolllllccclllc;;;;;::ccldxxdc::cclllodddlc::cloooooooollc:,:odddooooooooooolcccloodoooooodddxxxxddkXMMMMMMMM
+// MMMMMMMMMNxc;:ccclllllllllllcc:::ccclllooooooooooooooolllccccc:cclodxdolcllllooodoolllooddddooollc::,,;lxxdooooooooloolcclloooooooooddxxxxxdkXMMMMMMMM
+// MMMMMMMMNd:;::cccllllllllllllc:;;;;;::clloooooooooooddoooddddooollodxxdooollloodddddddddoddooollcc:;;,';ldddooooolllloolccllooooooooodddxxxkXWMMMMMMMM
+// MMMMMMMMKl;:::ccclllllllllccccc::;;;::cclloolooooooodddddddddddddoodxxddooooooddddddooooooooolllccc::;,',coddoooollollloolcclloooooooodddxOXMMMMMMMMMM
+// MMMMMMMMW0l:::ccccllloooolccccc::;;:::clloolclloooooodddddddddddddddddddooooddddddooooooooooollllccc::;,'':lddooooolllllloollcllloooooddkKWMMMMMMMMMMM
+// MMMMMMMMMWXkl:ccccllloooollcccc::;:::clllllcclllllloooddddddddddddddddddooodddooooooooooollolllllccccc::;,';cloooooolloooodddocclllloox0NMMMMMMMMMMMMM
+// MMMMMMMMMMMWXklcccllllllllcccc::;;::ccllllcccllllooooodddddddxxxxxxxxdddoodddddoooooooooollllllllcccccc::;,,';:lloddddddddddddlcclloxKNMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMWXOdlcclcccccllcc:;;:::ccclllllloooooooddddddddddddxxxxxdddddddodooooooooooooooollllllcccc:::;;''',;coooddddddddoc:cdOXWMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMWKkdl::ccccc:;;;::::ccclllllooooooooodddddddddddddxxdddddddoooddooooooooooooooolllllccccc:::;;,'',;:cclloolllccd0NMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMWXOdlc:::;;;::::ccccclllloooooooooddddddddddddddddddddddddddxddooooooooooooooolllllccccc::::;;,,,,,;;:::cdOXWMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMWN0kdc;;:::::ccccccllllooooddddddddddddddoooooooooooddddddxxddoooodoooooooolllllllllllcccccccccccloxOXWMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMWXOxoc::::cccclllloooooddxxxxxdddddoooooolllllllloooooddxxddoddoooooooooollllllllllllloooodxOKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNKOdlc::cclllloooooodddxxxxddooooooolllllllllooooddddxxdddddooooooooollllllllllllloxk0XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKOkdolllllllllloooddddddoooooollllloooooooooodddddddooooooooooooollllllllodkOKNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK0OkxdoolloodddddddoooooooooooooooooddooddddooooooollllloollloodxO0XNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXXK0OOOkkxxxdddooooooooooooooooooooolllllllloooooodxkO0KXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWNNNNXXXXK jvvru://kdd.eq/s0X6743 KKKKKXXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+//                               The greatest ruses ever devised, are those where the truth was right in  front  the whole time.
+//                                                           These great strategists know best.
+
+//                                                The ignorant will explore to no  end  in search for more, 
+//                                                      The wise will cherish what is given to them
+
+let cover, page, pageTurned;
+
+function preload() {
+  cover = loadImage('assets/cover2.png');
+  page = loadImage('assets/page2.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pageTurned = false;
+}
+
+function mousePressed() {
+  pageTurned = !pageTurned;
+}
+
+function draw() {
+  background(220);
+  
+  if(pageTurned) {
+    image(page, 0, 0, width, height);
+  } else {
+    image(cover, 0, 0, width, height);
+  }
+  
+}
